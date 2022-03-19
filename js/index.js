87ae86a61ac5d4b645985387823058eae973db6a
Nurbolot Boobekov <nurbolot.boobekov.pls@gmail.com>
2022-03-19 17:41:36 +0300

    fixed bug

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
-let burger = document.querySelector(".header__burger");
-let menu = document.querySelector(".menu");
-let btns = document.querySelector(".header__right");
-let loginBtn = document.querySelector(".header__login-btn");
-let body = document.querySelector("body");
+const burger = document.querySelector(".header__burger");
+const menu = document.querySelector(".menu");
+const btns = document.querySelector(".header__right");
+const body = document.querySelector("body");
+
+const obj = {
+  burger: [burger, "header__burger_active"],
+  menu: [menu, "menu_active"],
+  btns: [btns, "header__right_active"],
+  body: [body, "body_lock"],
+};
 
 burger.addEventListener("click", () => {
-  burger.classList.toggle("header__burger_active");
-  menu.classList.toggle("menu_active");
-  btns.classList.toggle("header__right_active");
-  //   loginBtn.classList.toggle("login-btn");
-  body.classList.toggle("body_lock");
+  for (key in obj) {
+    obj[key][0].classList.toggle(obj[key][1]);
+  }
 });
